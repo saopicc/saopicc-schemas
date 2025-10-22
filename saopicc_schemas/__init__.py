23366aeb3791620633b12a57ede1f448df3aee8e
Simon Perkins <simon.perkins@gmail.com>
2025-10-22 11:38:27 +0200

    Initial commit

---
+++ saopicc_schemas/__init__.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass, asdict
+from typing import Any, Dict, List, Literal
+import numpy as np
+from xarray_dataclasses import AsDataset
+from xarray_dataclasses import Attr, Coord, Data, Name, Dataof, Coordof
+
+#
+Singleton = tuple[()]
+
+# Dimensions
+Antenna = Literal["antenna"]
+Correlation = Literal["correlation"]
+Direction = Literal["direction"]
+GainTime = Literal["gain_time"]
+GainFreq = Literal["gain_freq"]
+TimeChunk = Literal["time_chunk"]
+FreqChunk = Literal["freq_chunk"]
+
+@dataclass
+class IntegrationTimeAttr:
+  """Integration Time attribute"""
+
+  data: Data[Singleton, float]
+  units: Attr[str] = "s"
+  type: Attr[str] = "quantity"
+
+
+@dataclass
+class GainTimeAxis:
+  """Define the gain_time coordinate.
+  This structure matches the MSv4 time coordinate axis
+  by included astropy time scale attributes"""
+  data: Data[GainTime, np.float64]
+  # This matches the MSv4 "integration_time" attribute
+  # but it may be superfluous and could be removed
+  integration_time: Attr[IntegrationTimeAttr] = None
+  type: Attr[str] = "time"
+  units: Attr[str] = "s"
+  format: Attr[str] = "unix"
+  scale: Attr[str] = "utc"
+
+
+@dataclass
+class Gains(AsDataset):
+  # Data Variables
+  conv_iter: Data[tuple[TimeChunk, FreqChunk], np.int64]
+  conv_perc: Data[tuple[TimeChunk, FreqChunk], np.float64]
+  gain_flags: Data[tuple[GainTime, GainFreq, Antenna, Direction], np.int8]
+
+  # Coordinates
+  antenna: Coord[Antenna, str]
+  correlation: Coord[Correlation, str]
+  direction: Coord[Direction, int]
+  time_chunk: Coord[TimeChunk, int]
+  freq_chunk: Coord[FreqChunk, int]
+  gain_time: Coordof[GainTimeAxis]
+  gain_freq: Coord[GainFreq, np.float64]
+
+  # Attributes
+  GAIN_AXES: Attr[List[str]]
+  GAIN_SPEC: Attr[List[List[int]]]
+  NAME: Attr[str]
+  TYPE: Attr[str]
+
+  # Version of the schema
+  VERSION: Attr[str] = "0.0.1"
---
+++ tests/test_basic.py
@@ -0,0 +1,69 @@
+from dataclasses import asdict
+
+import numpy as np
+import numpy.testing as npt
+import xarray
+
+from saopicc_schemas import Gains, GainTimeAxis
+
+def test_gains_creation():
+  time = np.linspace(1.0, 2.0, 10)
+  freqs = np.linspace(.856e9, 2 * .856e9, 16)
+  antenna = np.arange(28).astype(str)
+  direction = np.arange(1)
+  corrs = np.array(["RR", "RL", "LR", "LL"])
+  ntime = len(time)
+  nfreq = len(freqs)
+  nant = len(antenna)
+  ndir = len(direction)
+  conv_iter=np.full((4, 4), 4)
+  conv_perc=np.full((4, 4), 5)
+  gain_flags = np.random.choice([0, 1], (ntime, nfreq, nant, ndir))
+
+  gains = Gains.new(
+    conv_iter=conv_iter,
+    conv_perc=conv_perc,
+    time_chunk=np.arange(conv_iter.shape[0]),
+    freq_chunk=np.arange(conv_iter.shape[1]),
+    gain_flags=gain_flags,
+    antenna=antenna,
+    correlation=corrs,
+    direction=direction,
+    gain_time=xarray.DataArray(time, attrs={
+      "type": "time",
+      "units": "s",
+      "format": "unix",
+      "scale": "utc",
+      "integration_time": {
+        "attrs": {"type": "quantity", "units": "s"},
+        "data": 8.0,
+      }
+    }),
+    gain_freq=freqs,
+    GAIN_AXES=["gain_time", "gain_freq", "antenna", "direction", "correlation"],
+    GAIN_SPEC=[[115], [64], [28], [1], [4]],
+    NAME="G",
+    VERSION="0.0.1",
+    TYPE="complex",
+  )
+
+  # Test some data variables
+  npt.assert_array_equal(gains.data_vars["gain_flags"], gain_flags)
+  npt.assert_array_equal(gains.data_vars["conv_iter"], conv_iter)
+  npt.assert_array_equal(gains.data_vars["conv_perc"], conv_perc)
+
+  # Test some coordinates
+  npt.assert_array_equal(gains.coords["antenna"], antenna)
+  npt.assert_array_equal(gains.coords["direction"], direction)
+
+  # Test some attributes
+  assert "GAIN_AXES" in gains.attrs
+  assert "GAIN_SPEC" in gains.attrs
+  assert gains.attrs["NAME"] == "G"
+  assert "VERSION" in gains.attrs
+
+  # TODO(sjperkins)
+  # This seems to be the only way to correctly
+  # set the integration time attribute
+  gains.gain_time.attrs["integration_time"] = asdict(GainTimeAxis(8.0))
+  print(gains)
